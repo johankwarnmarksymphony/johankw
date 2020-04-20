a9ad5df188fd872ca0ff7c0a867bb5ac0628a785
Johan Kwarnmark <johan.kwarnmark@symphony.com>
2020-04-20 13:41:27 +0200

    .

--- crone_job_start.sh
+++ crone_job_start.sh
@@ -3,6 +3,6 @@ echo "START $(date)" >> a.txt
 echo "pwd : $(pwd)"
 echo "arg1: $1"
 
-python3 bellman_jira.py $1 $2 >> a.txt
+/usr/local/bin/python3 bellman_jira.py $1 $2 >> a.txt
 
 echo "END $(date)" >> a.txt
