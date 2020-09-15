b0629ac0fb71aa1741cc81c9ef4bf0de877c35f6
Johan Kwarnmark <johan.kwarnmark@symphony.com>
2020-09-15 15:33:39 +0200

    New bot collecting open C2 e2e bugs

---
+++ sfe_lite_e2e_bugs.py
@@ -0,0 +1,94 @@
+import sys
+import requests
+import json
+import time
+import datetime
+import humanfriendly
+from jira import JIRA, JIRAError
+
+NEW_LINE   = '<br/>\n'
+BOLD       = '<b>'
+BOLD_RESET = '</b>'
+
+###
+###
+###
+def usage():
+    print('')
+    print('  python ' + sys.argv[0])
+    print('')
+    print('  (' + str(sys.argv) + ')')
+    print('')
+
+    sys.exit(3)
+
+
+###
+###
+###
+def init_jira(user, apikey):
+    # Connect to JIRA
+    server = 'https://perzoinc.atlassian.net'
+
+    options = {
+        'server': server
+    }
+
+    return JIRA(options, basic_auth=(user, apikey))
+
+
+###
+###
+###
+def get_jira(jira, jira_project, jql):
+    jql = 'project = ' + jira_project + ' AND ' + jql
+
+    tickets = jira.search_issues(jql, maxResults=None)
+
+    subject_text = 'Open E2E testing bugs (' + str(len(tickets)) + ')'
+
+    body_text = ''
+    for x in tickets: 
+        body_text += '  ' + '{:15}'.format(str(x.fields.status)) + ' <a href=\"https://perzoinc.atlassian.net/browse/' + str(x) + '\" /> ' + x.fields.summary + NEW_LINE
+
+    return [subject_text, body_text]
+
+
+###
+###
+###
+def send_message_to_symphony(subject, body, webhook):
+    data = '<messageML><b>' + subject + '</b>' + NEW_LINE + body + '</messageML>'
+
+    print('data: ' + data)
+
+    if webhook == '':
+        return
+
+    r = requests.post(webhook, data=data)
+
+    print('r: ' + str(r))
+    print('r.status_code: ' + str(r.status_code))
+
+
+
+####################################################
+
+
+if len(sys.argv) == 4:
+    jira_user  = sys.argv[1]
+    jira_token = sys.argv[2] 
+    webhook    = sys.argv[3]
+else:
+    usage()
+    sys.exit(37)
+
+jira = init_jira(jira_user, jira_token)
+
+[subject_text, body_text] = get_jira(jira, 'c2', 'project = \"C2\" and labels = \"c2-e2e-test-fail\" ORDER BY created DESC')
+
+#print('subject_text: ' + subject_text)
+#print('body_text: ' + body_text)
+
+send_message_to_symphony(subject_text, body_text, webhook)
+
