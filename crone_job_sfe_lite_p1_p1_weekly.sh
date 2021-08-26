c0a26983e2401bf1ada5e399298d64282188b5d1
Johan Kwarnmark <johan.kwarnmark@symphony.com>
2021-08-26 13:48:48 +0200

    Correct python file name

--- crone_job_sfe_lite_p1_p1_weekly.sh
+++ crone_job_sfe_lite_p1_p1_weekly.sh
@@ -3,6 +3,6 @@ echo "START $(date)" >> a.txt
 echo "pwd : $(pwd)"
 echo "args: $*"
 
-/usr/local/bin/python3 sfe_lite_p1_p1_weekly.py $* >> a.txt
+/usr/local/bin/python3 sfe_lite_p1_weekly.py $* >> a.txt
 
 echo "END $(date)" >> a.txt
